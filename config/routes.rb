bb127189babcf9cd2bd3c32469c001d75f10a7eb
Alyssa <ajkruegs@outlook.com>
2017-03-06 09:22:36 -0600

    adding bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'fight/index'
-
-  get 'fight_controller/index'
+  get '/', to: 'fight#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
