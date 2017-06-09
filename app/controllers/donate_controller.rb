e9b7cb6e962b4097b79e81aaed7e283f6a956eac
rtaber88 <ryan.n.taber@gmail.com>
2017-06-08 22:15:19 -0500

    lots of changes - front and back end

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -1,4 +1,6 @@
 class DonateController < ApplicationController
+  
+
   def index
   	@current_user = current_user
   end
@@ -10,11 +12,29 @@ class DonateController < ApplicationController
   end
 
   def select_food
+    respond_to do |format|
+      format.js
+    end
   end
   
+ def select_friend
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def throw_food
+    respond_to do |format|
+      format.js
+    end
   end
   
+  def confirm_throw
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def donation
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   get '/sign_in', to: 'donate#sign_in'
   get '/index', to: 'donate#index'
   get '/about', to: 'donate#about'
-  
+  get '/select_food', to: 'donate#select_food'
+  get '/select_friend', to: 'donate#select_friend'
+  get '/throw_food', to: 'donate#throw_food'
+  get '/confirm_throw', to: 'donate#confirm_throw'
   root 'donate#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
